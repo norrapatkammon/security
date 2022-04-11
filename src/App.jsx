82fd1759e7be85177eeaa5d54f785d74b344b971
Devmonter <89644054+devmonter@users.noreply.github.com>
2022-04-11 11:06:11 +0700

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react"
+import { Route, Switch } from "react-router-dom";
 import './App.scss'
 import Navbar from './component/navbar'
 import Monoalphabetic from './component/monoalphabetic'
 import Rsa from './component/rsa'
 import Rail_fence from './component/Rail_fence'
 import Shift_ciper from './component/shift_ciper'
-import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Navbar/>       
-          <Route exact path="/" >
-            <Monoalphabetic/>
-          </Route>
-      <Rsa/>
-      <Shift_ciper/>
-      <Rail_fence/>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Monoalphabetic}/>
+        <Route path="/rsa" component={Rsa}/>
+        <Route path="/shift_ciper" component={Shift_ciper}/>
+        <Route path="/rail_fence" component={Rail_fence}/>
+      </Switch>    
     </>
   )
 }
